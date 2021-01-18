4907c3306fe2b38fe09a1673fb5872447832725a
Heath Ward <heathw24@outlook.com>
2021-01-18 17:19:29 -0500

    fixed get request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,33 @@
 
 var fs = require("fs");
 
-var notes = fs.readFile("db.json");
 
 
 module.exports = function(app) {
 
+    //========read db.json file and return all notes as json ===========================
     app.get("/api/notes", function (req, res) {
-        res.notes;
-        console.log(notes);
+        fs.readFile("db/db.json", function(err, data) {
+           if (err) throw err;
+           console.log(JSON.parse(data));
+           res.json(JSON.parse(data));
+        });
+        
     });
 
+    //=========receive new note to save on request body, add it to db.json, return new note to client=======
     app.post("/api/notes", function(req, res) {
-        notes.push(req.body);
-
+        console.log(req.body);
+        fs.readFile("db/db.json", function(err, data) {
+            if (err) throw err;
+            let notes = JSON.parse(data);
+            console.log(notes);
+            notes.push(req.body);
+            console.log(notes);
+            res.json(req.body);
+         });
     });
+
+    //======= delete note given ID ======================
+
 }
